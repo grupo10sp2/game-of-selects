274fd286d30c39e11d844000da7c2f189c93d648
william-kokubun <william.kokubun@sptech.school>
2022-11-15 17:25:49 -0300

    inserindo valores na tabela estadioJogo

--- scriptCopaDoMundo.sql
+++ scriptCopaDoMundo.sql
@@ -582,6 +582,8 @@ insert into estadio (nome, cidade) values
 ('Al Thumama','Doha'),
 ('Al Janoub','Al Wakrah');
 
+select * from estadio;
+
 insert into arbitragem (nomeArbitroOficial, nomeBandeirinha1, nomeBandeirinha2) values
 ("Abdulrahman Al-Jassim","Taleb Al Marri","Saoud Al-Maqaleh"),
 ("Chris Beath","Anton Shchetinin","Ashley Beecham"),
@@ -750,6 +752,58 @@ insert into selecoesPartida (fkPartida, fkSelecao1, fkSelecao2, golsSelecao1, go
 (46, 29, 30, 2, 2),
 (47, 10, 27, 2, 3),
 (48, 18, 32, 1, 4);
+
+insert into estadioJogo (fkPartida, fkEstadio) values
+(1,  1),
+(2,  2),
+(3,  3),
+(4,  4),
+(5,  5),
+(6,  6),
+(7,  7),
+(8,  8),
+(9,  1),
+(10, 2),
+(11, 3),
+(12, 4),
+(13, 5),
+(14, 6),
+(15, 7),
+(16, 8),
+(17, 1),
+(18, 2),
+(19, 3),
+(20, 4),
+(21, 5),
+(22, 6),
+(23, 7),
+(24, 8),
+(25, 1),
+(26, 2),
+(27, 3),
+(28, 4),
+(29, 5),
+(30, 6),
+(31, 7),
+(32, 8),
+(33, 1),
+(34, 2),
+(35, 3),
+(36, 4),
+(37, 5),
+(38, 6),
+(39, 7),
+(40, 8),
+(41, 1),
+(42, 2),
+(43, 3),
+(44, 4),
+(45, 5),
+(46, 6),
+(47, 7),
+(48, 8);
+
+
 
 
 
